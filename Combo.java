1186ea7f4527bd1d94982103afbffd081f271051
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-08-25 14:04:41 -0400

    Update Combo.java

--- Combo.java
+++ Combo.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.io.*;
 
@@ -17,8 +16,8 @@ public class Combo {
 	}
 	
 	public static void build() {
-		for (int i=0; i<=n; i++) {
-			for (int j=0; j<=k && j<=i; j++) {
+		for (int i=0; i<=choose.length; i++) {
+			for (int j=0; j<=choose[0].length && j<=i; j++) {
 				if (j == 0 || j == i) choose[i][j] = 1;
 				else {
 					choose[i][j] = choose[i-1][j-1] + choose[i-1][j];
