0c922d8606fe26b7af25296b316942ce03295d69
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-09-01 17:50:48 -0400

    Update Cryptography.java

--- SegmentTree/Cryptography.java
+++ SegmentTree/Cryptography.java
@@ -45,127 +45,6 @@ public class Cryptography {
 		}
 		System.out.println(sb);
 	}
-
-	static class Reader 
-    { 
-        final private int BUFFER_SIZE = 1 << 16; 
-        private DataInputStream din; 
-        private byte[] buffer; 
-        private int bufferPointer, bytesRead; 
-  
-        public Reader() 
-        { 
-            din = new DataInputStream(System.in); 
-            buffer = new byte[BUFFER_SIZE]; 
-            bufferPointer = bytesRead = 0; 
-        } 
-  
-        public Reader(String file_name) throws IOException 
-        { 
-            din = new DataInputStream(new FileInputStream(file_name)); 
-            buffer = new byte[BUFFER_SIZE]; 
-            bufferPointer = bytesRead = 0; 
-        } 
-  
-        public String readLine() throws IOException 
-        { 
-            byte[] buf = new byte[64]; // line length 
-            int cnt = 0, c; 
-            while ((c = read()) != -1) 
-            { 
-                if (c == '\n') 
-                    break; 
-                buf[cnt++] = (byte) c; 
-            } 
-            return new String(buf, 0, cnt); 
-        } 
-  
-        public int nextInt() throws IOException 
-        { 
-            int ret = 0; 
-            byte c = read(); 
-            while (c <= ' ') 
-                c = read(); 
-            boolean neg = (c == '-'); 
-            if (neg) 
-                c = read(); 
-            do
-            { 
-                ret = ret * 10 + c - '0'; 
-            }  while ((c = read()) >= '0' && c <= '9'); 
-  
-            if (neg) 
-                return -ret; 
-            return ret; 
-        } 
-  
-        public long nextLong() throws IOException 
-        { 
-            long ret = 0; 
-            byte c = read(); 
-            while (c <= ' ') 
-                c = read(); 
-            boolean neg = (c == '-'); 
-            if (neg) 
-                c = read(); 
-            do { 
-                ret = ret * 10 + c - '0'; 
-            } 
-            while ((c = read()) >= '0' && c <= '9'); 
-            if (neg) 
-                return -ret; 
-            return ret; 
-        } 
-  
-        public double nextDouble() throws IOException 
-        { 
-            double ret = 0, div = 1; 
-            byte c = read(); 
-            while (c <= ' ') 
-                c = read(); 
-            boolean neg = (c == '-'); 
-            if (neg) 
-                c = read(); 
-  
-            do { 
-                ret = ret * 10 + c - '0'; 
-            } 
-            while ((c = read()) >= '0' && c <= '9'); 
-  
-            if (c == '.') 
-            { 
-                while ((c = read()) >= '0' && c <= '9') 
-                { 
-                    ret += (c - '0') / (div *= 10); 
-                } 
-            } 
-  
-            if (neg) 
-                return -ret; 
-            return ret; 
-        } 
-  
-        private void fillBuffer() throws IOException 
-        { 
-            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
-            if (bytesRead == -1) 
-                buffer[0] = -1; 
-        } 
-  
-        private byte read() throws IOException 
-        { 
-            if (bufferPointer == bytesRead) 
-                fillBuffer(); 
-            return buffer[bufferPointer++]; 
-        } 
-  
-        public void close() throws IOException 
-        { 
-            if (din == null) 
-                return; 
-            din.close(); 
-        } 
-    }
 	
 	static class Matrix {
 		long[][] grid;
@@ -236,18 +115,132 @@ public class Cryptography {
 			ans[0][1] = (a.grid[0][0] * b.grid[0][1] + a.grid[0][1] * b.grid[1][1])%r;
 			ans[1][0] = (a.grid[1][0] * b.grid[0][0] + a.grid[1][1] * b.grid[1][0]) % r;
 			ans[1][1] = (a.grid[1][0] * b.grid[0][1] + a.grid[1][1] * b.grid[1][1])%r;
-//			for (int i=0; i<2; i++) {
-//				for (int j=0; j<2; j++) {
-//					for (int k=0; k<2; ++k) {
-//						ans[i][k] += a.grid[i][j] * b.grid[j][k] % r;
-//						ans[i][k] %= r;
-//					}
-//				}
-//			}
 			Matrix prod = new Matrix(ans);
 			return prod;
 		}
 	}
+	
+	// source: https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
+	static class Reader { 
+		final private int BUFFER_SIZE = 1 << 16; 
+		private DataInputStream din; 
+		private byte[] buffer; 
+		private int bufferPointer, bytesRead; 
+
+		public Reader() 
+		{ 
+		    din = new DataInputStream(System.in); 
+		    buffer = new byte[BUFFER_SIZE]; 
+		    bufferPointer = bytesRead = 0; 
+		} 
+
+		public Reader(String file_name) throws IOException 
+		{ 
+		    din = new DataInputStream(new FileInputStream(file_name)); 
+		    buffer = new byte[BUFFER_SIZE]; 
+		    bufferPointer = bytesRead = 0; 
+		} 
+
+		public String readLine() throws IOException 
+		{ 
+		    byte[] buf = new byte[64]; // line length 
+		    int cnt = 0, c; 
+		    while ((c = read()) != -1) 
+		    { 
+			if (c == '\n') 
+			    break; 
+			buf[cnt++] = (byte) c; 
+		    } 
+		    return new String(buf, 0, cnt); 
+		} 
+
+		public int nextInt() throws IOException 
+		{ 
+		    int ret = 0; 
+		    byte c = read(); 
+		    while (c <= ' ') 
+			c = read(); 
+		    boolean neg = (c == '-'); 
+		    if (neg) 
+			c = read(); 
+		    do
+		    { 
+			ret = ret * 10 + c - '0'; 
+		    }  while ((c = read()) >= '0' && c <= '9'); 
+
+		    if (neg) 
+			return -ret; 
+		    return ret; 
+		} 
+
+		public long nextLong() throws IOException 
+		{ 
+		    long ret = 0; 
+		    byte c = read(); 
+		    while (c <= ' ') 
+			c = read(); 
+		    boolean neg = (c == '-'); 
+		    if (neg) 
+			c = read(); 
+		    do { 
+			ret = ret * 10 + c - '0'; 
+		    } 
+		    while ((c = read()) >= '0' && c <= '9'); 
+		    if (neg) 
+			return -ret; 
+		    return ret; 
+		} 
+
+		public double nextDouble() throws IOException 
+		{ 
+		    double ret = 0, div = 1; 
+		    byte c = read(); 
+		    while (c <= ' ') 
+			c = read(); 
+		    boolean neg = (c == '-'); 
+		    if (neg) 
+			c = read(); 
+
+		    do { 
+			ret = ret * 10 + c - '0'; 
+		    } 
+		    while ((c = read()) >= '0' && c <= '9'); 
+
+		    if (c == '.') 
+		    { 
+			while ((c = read()) >= '0' && c <= '9') 
+			{ 
+			    ret += (c - '0') / (div *= 10); 
+			} 
+		    } 
+
+		    if (neg) 
+			return -ret; 
+		    return ret; 
+		} 
+
+		private void fillBuffer() throws IOException 
+		{ 
+		    bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+		    if (bytesRead == -1) 
+			buffer[0] = -1; 
+		} 
+
+		private byte read() throws IOException 
+		{ 
+		    if (bufferPointer == bytesRead) 
+			fillBuffer(); 
+		    return buffer[bufferPointer++]; 
+		} 
+
+		public void close() throws IOException 
+		{ 
+		    if (din == null) 
+			return; 
+		    din.close(); 
+		} 
+	    }
+	
 }
 /*
 	Keep track of multiplication within the tree instead of addition
