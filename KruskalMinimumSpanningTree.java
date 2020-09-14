d809ca32a6e2cec229aba3167e2ddaa7545ab196
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-09-14 19:17:40 -0400

    Add files via upload

---
+++ KruskalMinimumSpanningTree.java
@@ -0,0 +1,95 @@
+
+import java.util.*;
+import java.io.*;
+
+public class KruskalMinimumSpanningTree {
+
+	static int n, m;
+	static long INF = (long)(1e18);
+	static Edge[] edges;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader in = new BufferedReader(new FileReader("Dijkstras"));
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		n = Integer.parseInt(st.nextToken()); 	// number of vertices
+		m = Integer.parseInt(st.nextToken()); 	// number of edges
+		edges = new Edge[m];
+				
+		for (int i=0; i<m; i++) {
+			st = new StringTokenizer(in.readLine());
+			int a = Integer.parseInt(st.nextToken())-1; 	
+			int b = Integer.parseInt(st.nextToken())-1; 	
+			long c = Long.parseLong(st.nextToken()); 
+			edges[i] = new Edge(a,b,c);
+		}
+		
+		Arrays.parallelSort(edges);
+		
+		dsu s = new dsu(n);
+		int ans = MST(s);
+		
+	}
+	
+	public static int MST(dsu s) {
+		int ans=0;
+		for (int i=0; i<m; i++) {
+			if (s.Union(edges[i].from, edges[i].destination)) {
+				ans += edges[i].length;
+			}
+		}
+		return ans;
+	}
+	
+	static class Edge implements Comparable<Edge> {
+		int from;
+		int destination;
+		long length;
+		Edge(int a , int b, long c) {
+			from = a;
+			destination = b;
+			length = c;
+		}
+		
+		public int compareTo(Edge o) {
+			return Long.compare(length, o.length);
+		}
+	}
+	
+	static class dsu {
+		int n;
+		int[] parent;
+		int[] size;
+		
+		dsu (int n) {
+			this.n = n;
+			parent = new int[n];
+			size = new int[n];
+			for (int i=0; i<n; i++) {parent[i] = i; size[i] = 1;}
+		}
+
+		public int FindSet(int a) {
+			if (a == parent[a]) return a;
+			return parent[a] = FindSet(parent[a]);
+		}
+		
+		public boolean Union(int a, int b) {
+			a = FindSet(a);
+			b = FindSet(b);	
+			if (a == b) { 			// already grouped
+				return false;
+			}
+			
+			if (size[a] < size[b]) {
+				parent[a] = b;
+				size[b] += size[a];
+			}
+			else {
+				parent[b] = a;
+				size[a] += size[b];
+			}
+			return true;
+		}
+	}
+}
