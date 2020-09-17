924da7c069c2730eea164694afa4688f0e28d589
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-09-17 16:51:48 -0400

    Add files via upload

---
+++ BinaryIndexTree.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+import java.io.*;
+
+public class BinaryIndexTree {
+
+	// https://cp-algorithms.com/data_structures/fenwick.html
+	
+	public static void main(String[] args) {
+
+	}
+	
+	
+	static class BIT {
+		int n;
+		long[] f;	// 1 base indexing
+		BIT (int n) {
+			this.n = n+1; f = new long[n+1];
+		}
+		
+		long sum (int i) {		// sum from 0 to i
+			long ret=0; i++;
+			while (i>0) {
+				ret += f[i];
+				i -= i&-i;
+			}
+			return ret;
+		}
+		
+		long sum (int l, int r) {	// sum from l to r
+			l++; r++;
+			return sum(r) - sum(l-1);
+		}
+		
+		void add(int i, long value) {	// add value to index i
+			i++;
+			while (i<=n) {
+				f[i] += value;
+				i += i&-i;
+			}
+		}
+		
+		void add(int l, int r, long value) {	// add value to indices l to r
+			l++; r++;
+			add(l,value); add(r+1,-value);
+		}
+	}
+}
