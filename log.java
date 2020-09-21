83c66d0c140a21df11362be580049f8e3e2b0fce
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-09-21 18:39:56 -0400

    Update log.java

--- log.java
+++ log.java
@@ -20,17 +20,13 @@ public class log {
 	
 	public static long log(long n) {
 		long x = 1;
-		while ((1l<<(x+1)) <= n) {
-			x++;
-		}
+		while ((1l<<(x+1)) <= n) x++;
 		return x;
 	}
 	
 	public static int log(int n) {
 		int x = 1;
-		while ((1<<(x+1)) <= n) {
-			x++;
-		}
+		while ((1<<(x+1)) <= n) x++;
 		return x;
 	}
 }
