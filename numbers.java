63538c95700e168dc25cf0ff6f843827ffb839d3
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-09-27 10:53:51 -0400

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -60,8 +60,12 @@ public class numbers {
 	}
 	
 	public static boolean prime(int n) {
-		ArrayList<Integer> a = getDivisors(n);
-		if (a.size() == 2) return true;
-		return false;
+		if (n<=1) return false;
+		if (n<=3) return true;
+		if (n%2==0 || n%3==0) return false;
+		for (int i=5; i*i <= n; i+=6) {
+			if (n%i==0 || n%(i+2)==0) return false;
+		}
+		return true;
 	}
 }
