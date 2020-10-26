e2bb9961fe8e36fd629a2832aaa1cf6f8029cc57
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-10-26 14:22:07 -0400

    Update binexponentation.java

--- binexponentation.java
+++ binexponentation.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.io.*;
 
@@ -7,32 +6,31 @@ public class binexponentation {
 
 	}
 	
-	static long pow(long a, long b) {
-        // a^b
-    	long ans=1;
-    	while (b >0) {
+    // a^b
+	public static long pow(long a, long b) {
+    	long ans = 1;
+    	while (b > 0) {
     		if (b%2 == 1) {
     			ans *= a;
     		}
     		a *= a;
-    		b >>=1;
+    		b >>= 1;
     	}
     	return ans;
     }
 	
-	static long pow(long a, long b, long m) {
-        // a^b mod m
-    	long ans=1;
-    	while (b >0) {
+    // a^b mod m
+	public static long pow(long a, long b, long m) {
+    	long ans = 1;
+    	while (b > 0) {
     		if (b%2 == 1) {
-    			ans *= a%m;
+    			ans *= a;
     			ans %= m;
     		}
-    		a *= a %m;
-    		a%=m;
-    		b >>=1;
+    		a *= a;
+    		a %= m;
+    		b >>= 1;
     	}
     	return ans;
-    }
-	
+    }	
 }
