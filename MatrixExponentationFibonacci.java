391ae1ca7b3c5f119e118ea46e7b48606385c460
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-11-03 19:40:35 -0500

    Update MatrixExponentationFibonacci.java

--- MatrixExponentationFibonacci.java
+++ MatrixExponentationFibonacci.java
@@ -16,19 +16,19 @@ public class MatrixExponentationFibonacci {
 		long[][] ans = {{0,1}, {1,1}};
 		while (n > 0) {
 			if (n%2 == 1) {
-				ans = multiply(ans, arr);
+				ans = multiply(2, ans, arr);
 			}
-			arr = multiply(arr, arr);
+			arr = multiply(2, arr, arr);
 			n >>= 1;
 		}
 		System.out.println(ans[0][0]%mod);
 	}
 	
-	public static long[][] multiply (long[][] a, long[][] b) {
-		long[][] product = new long[2][2];
-		for (int i=0; i<2; i++) {
-			for (int j=0; j<2; j++) {
-				for (int k=0; k<2; k++) {
+	public static long[][] multiply (int n, long[][] a, long[][] b) {
+		long[][] product = new long[n][n];
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				for (int k=0; k<n; k++) {
 					product[i][k] += a[i][j] * b[j][k];
 					product[i][k] %= mod;
 				}
