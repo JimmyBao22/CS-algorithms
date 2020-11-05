ea4de5b85bc3e5d4940b5d71de4731fb8d7cc576
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-11-05 01:01:59 -0500

    Update Combo.java

--- Combo.java
+++ Combo.java
@@ -8,11 +8,21 @@ public class Combo {
 	static long mod;
 	static long[][] choose;
 	
+	static long[] fact, inv_fact;
+	
 	public static void main(String[] args) {
 		
 		choose = new long[n+1][k+1];
 		build();
 		
+		fact = new long[n + k + 1]; 
+		inv_fact = new long[n + k + 1];
+		fact[0] = inv_fact[0] = 1;
+		for (int i=1; i<fact.length; i++) {
+			fact[i] = fact[i-1] * i;
+			fact[i] %= mod;
+			inv_fact[i] = pow(fact[i], mod-2, mod);
+		}	
 	}
 	
 	public static void build() {
@@ -27,4 +37,23 @@ public class Combo {
 		}
 	}
 	
+	public static long choose(int top, int bottom) {
+		// top! / bottom! (top - bottom)!
+		return fact[top] * inv_fact[bottom] %mod * inv_fact[top - bottom] %mod;
+	}
+	
+    static long pow(long a, long b, long m) {
+        // a^b
+    	long ans=1;
+    	while (b >0) {
+    		if (b%2 == 1) {
+    			ans *= a%m;
+    			ans %= m;
+    		}
+    		a *= a %m;
+    		a%=m;
+    		b >>=1;
+    	}
+    	return ans;
+    }
 }
