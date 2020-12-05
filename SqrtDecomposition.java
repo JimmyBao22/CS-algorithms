32d2c305f8bdadc768bab7111e03434decd6b3a1
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-04 22:51:58 -0500

    Update SqrtDecomposition.java

--- SqrtDecomposition.java
+++ SqrtDecomposition.java
@@ -6,8 +6,6 @@ public class SqrtDecomposition {
 
 	static int n;
 	static long[] arr;
-	static int len;				// size of blocks and # of blocks
-	static long[] b;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -15,50 +13,59 @@ public class SqrtDecomposition {
 		
 		n = Integer.parseInt(in.readLine());
 		arr = new long[n];
-		len = (int) (Math.sqrt(n) + 1);
-		b = new long[len];
 		
 		StringTokenizer st = new StringTokenizer(in.readLine());
 		for (int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			b[i/len] += arr[i];
 		}	
 		
 	}
 	
-	public static void update(int i, long x) {
-		int block = i/len;
-		b[block] += x - arr[i];
-		arr[i] = x;
-	}
-	
-	public static long query(int l, int r) {
-		long sum=0;
-		int left = l/len; int right = r/len;
-		if (left == right) {
-			for (int i=l; i<=r; i++) sum += arr[i];
+	static class SqrtDecomp {
+		
+		int len;				// size of blocks and # of blocks
+		long[] b;
+		
+		public SqrtDecomp() {
+			len = (int) (Math.sqrt(n) + 1);
+			b = new long[len];
+			for (int i=0; i<n; i++) b[i/len] += arr[i];
 		}
-		else {
-			for (int i=l; i<=(left + 1)*len - 1; i++) sum += arr[i];
-			for (int i=left+1; i<=right-1; i++) sum += b[i];
-			for (int i=right*len; i<=r; i++) sum += arr[i];
+		
+		public void update(int i, long x) {
+			int block = i/len;
+			b[block] += x - arr[i];
+			arr[i] = x;
 		}
-		return sum;
-	}
-	
-	public static void update_seg(int l, int r, long x) {
-		int left = l/len; int right = r/len;
-		if (left == right) {
-			for (int i=l; i<=r; i++) arr[i] += x;
+		
+		public long query(int l, int r) {
+			long sum=0;
+			int left = l/len; int right = r/len;
+			if (left == right) {
+				for (int i=l; i<=r; i++) sum += arr[i];
+			}
+			else {
+				for (int i=l; i<=(left + 1)*len - 1; i++) sum += arr[i];
+				for (int i=left+1; i<=right-1; i++) sum += b[i];
+				for (int i=right*len; i<=r; i++) sum += arr[i];
+			}
+			return sum;
 		}
-		else {
-			for (int i=l; i<=(left + 1)*len - 1; i++) arr[i] += x;
-			for (int i=left+1; i<=right-1; i++) b[i] += x;
-			for (int i=right*len; i<=r; i++) arr[i] += x;
+		
+		public void update_seg(int l, int r, long x) {
+			int left = l/len; int right = r/len;
+			if (left == right) {
+				for (int i=l; i<=r; i++) arr[i] += x;
+			}
+			else {
+				for (int i=l; i<=(left + 1)*len - 1; i++) arr[i] += x;
+				for (int i=left+1; i<=right-1; i++) b[i] += x;
+				for (int i=right*len; i<=r; i++) arr[i] += x;
+			}
 		}
-	}
-	
-	public static long query_index(int i) {
-		return arr[i] + b[i/len];
+		
+		public long query_index(int i) {
+			return arr[i] + b[i/len];
+		}
 	}
 }
