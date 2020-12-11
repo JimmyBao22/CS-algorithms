6b1191ce8ee7bc6d6e6d34a8836cde51f6ee4ef0
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-10 22:45:49 -0500

    Add files via upload

---
+++ EulerTour.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+import java.io.*;
+
+public class EulerTour {
+
+	static int n, time;
+	static ArrayList<Integer>[] g;
+	static int[] start, end;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader in = new BufferedReader(new FileReader("EulerTour"));
+
+		n = Integer.parseInt(in.readLine());
+		time = 0;
+		g = new ArrayList[n];
+		for (int i=0; i<n; i++) g[i] = new ArrayList<>();
+		for (int i=0; i<n-1; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int a = Integer.parseInt(st.nextToken())-1; 	
+			int b = Integer.parseInt(st.nextToken())-1;
+			g[a].add(b);
+			g[b].add(a);
+		}
+		
+		start = new int[n]; end = new int[n];
+		dfs(0, -1);
+		
+	}
+	
+	public static void dfs(int node, int p) {
+		start[node] = time; time++;
+		for (Integer i : g[node]) {
+			if (i != p) dfs(i, node);
+		}
+		end[node] = time-1;
+	}
+}
