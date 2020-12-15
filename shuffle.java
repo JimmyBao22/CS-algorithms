9540c7139de3041ab0d094b8b408bb73cd621083
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-15 11:46:17 -0500

    Update shuffle.java

--- shuffle.java
+++ shuffle.java
@@ -1,20 +1,9 @@
-
 import java.util.*;
 import java.io.*;
 
 public class shuffle {
 	public static void main(String[] args) {
-		int[] a = new int[] {1,2,3,4,5,6,7};
-		long[] b = new long[] {1l, 2l ,3l, 4l, 5l ,6l, 7l};
-		shuffle(a);
-		shuffle(b);
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i]);
-		}
-		System.out.println();
-		for (int i = 0; i < b.length; i++) {
-			System.out.print(b[i]);
-		}
+
 	}
 	
 	public static void shuffle(int[] a) {
@@ -26,24 +15,5 @@ public class shuffle {
             a[r] = temp;
         }
     }
- 
-    public static void shuffle(long[] a) {
-        Random get = new Random();
-        for (int i = 0; i < a.length; i++) {
-            int r = get.nextInt(a.length);
-            long temp = a[i];
-            a[i] = a[r];
-            a[r] = temp;
-        }
-    }
-    
-    public static void shuffle(char[] a) {
-        Random get = new Random();
-        for (int i = 0; i < a.length; i++) {
-            int r = get.nextInt(a.length);
-            char temp = a[i];
-            a[i] = a[r];
-            a[r] = temp;
-        }
-    }
+	
 }
