f4649bea2dda98a4aeae35310a99ba1e28fca542
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-17 14:25:35 -0500

    Update and rename shuffle.java to Shuffle.java

---
+++ Shuffle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class shuffle {
+	public static void main(String[] args) {
+
+	}
+	
+	public static void shuffle(int[] a) {
+        	Random get = new Random();
+		for (int i = 0; i < a.length; i++) {
+		    int r = get.nextInt(a.length);
+		    int temp = a[i];
+		    a[i] = a[r];
+		    a[r] = temp;
+		}
+    	}
+	
+}
