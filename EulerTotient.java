817fcfc92e48365ee825d5c3dced516ce10a27fd
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-26 22:59:36 -0500

    Update EulerTotient.java

--- EulerTotient.java
+++ EulerTotient.java
@@ -3,19 +3,19 @@ import java.io.*;
 
 public class EulerTotient {
 	
-	static int max = (int)1e5;
-	static int[] arr = new int[max];
+	static int MaxN = (int)1e5+10;
+	static int[] arr = new int[MaxN];
 	
 	public static void buildtotient() {
-		for (int i=1; i<max; i++) {
+		for (int i=1; i<MaxN; i++) {
 			arr[i] = i;
 		}
-		for (int i=2; i<max; i+=2) {
+		for (int i=2; i<MaxN; i+=2) {
 			arr[i] = arr[i]/2;
 		}
-		for (int i=3; i<max; i+=2) {
+		for (int i=3; i<MaxN; i+=2) {
 			if (arr[i] == i) {
-				for (int j= i; j < max; j+= i) {
+				for (int j= i; j < MaxN; j+= i) {
 					arr[j] /= i; arr[j] *= (i-1);
 				}
 			}
@@ -26,13 +26,14 @@ public class EulerTotient {
 		
 		buildtotient();
 		
-		for (int i=1; i<=100; i++) {
+		for (int i=1; i<=10000; i++) {
 			if (totient(i) != arr[i]) {
 				System.out.println("FALSE");
 				break;
 			}
 			System.out.println(arr[i]);
 		}
+		
 	}
 	
 	public static long totient(long n) {
@@ -44,19 +45,20 @@ public class EulerTotient {
 				ret *= 2;
 			}
 		}
-		for (long i = 3; i*i<=n; i+=2)  { 
-		    if (n%i==0) { 
-			ret *= (i-1);
-			n /= i;
-			while (n%i ==0) {
-				ret *= i;
-				n/=i;
-			}
-		    } 
-		}
-		if (n!=1) {
-			ret *= (n-1);
-		}
+        for (long i = 3; i*i<=n; i+=2)  { 
+            if (n%i==0) { 
+                ret *= (i-1);
+                n /= i;
+                while (n%i ==0) {
+                	ret *= i;
+                	n/=i;
+                }
+            } 
+        }
+        if (n!=1) {
+        	ret *= (n-1);
+        }
 		return ret;
-	}	
+	}
+	
 }
