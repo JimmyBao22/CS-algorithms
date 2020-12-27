dffac2b06a943e4a09bf0b72ca05fe42d5825b72
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2020-12-27 16:53:23 -0500

    Update SqrtDecomposition.java

--- SqrtDecomposition.java
+++ SqrtDecomposition.java
@@ -27,7 +27,7 @@ public class SqrtDecomposition {
 		long[] b;
 		
 		public SqrtDecomp() {
-			len = 32;
+			len = (int) (Math.sqrt(n) + 1);
 			b = new long[len];
 			for (int i=0; i<n; i++) b[i/len] += arr[i];
 		}
