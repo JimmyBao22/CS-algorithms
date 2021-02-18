a271dec2808afb250644dd9efb9a9a6dc78e640c
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-02-18 11:59:59 -0500

    Update EulerTotient.java

--- EulerTotient.java
+++ EulerTotient.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class EulerTotient {
 	
-	static int MaxN = (int)1e5+10;
+	static int MaxN = (int)1e5+2;
 	static int[] arr = new int[MaxN];
+	static ArrayList<Integer> primes = new ArrayList<>();
 	
 	public static void buildtotient() {
 		for (int i=1; i<MaxN; i++) {
@@ -15,6 +16,24 @@ public class EulerTotient {
 		}
 		for (int i=3; i<MaxN; i+=2) {
 			if (arr[i] == i) {
+				for (int j= i; j < MaxN; j+= i) {
+					arr[j] /= i; arr[j] *= (i-1);
+				}
+			}
+		}
+	}
+	
+	public static void getprimes() {
+		for (int i=1; i<MaxN; i++) {
+			arr[i] = i;
+		}		
+		primes.add(2);
+		for (int i=2; i<MaxN; i+=2) {
+			arr[i] = arr[i]/2;
+		}
+		for (int i=3; i<MaxN; i+=2) {
+			if (arr[i] == i) {
+				primes.add(i);
 				for (int j= i; j < MaxN; j+= i) {
 					arr[j] /= i; arr[j] *= (i-1);
 				}
@@ -60,5 +79,4 @@ public class EulerTotient {
         }
 		return ret;
 	}
-	
 }
