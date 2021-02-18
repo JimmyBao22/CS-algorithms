2929274607279425adcd51713c13928fe220bf7f
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-02-18 12:00:21 -0500

    Update Sieve.java

--- Sieve.java
+++ Sieve.java
@@ -1,12 +1,12 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Sieve {
 	
-	static int max = (int)1e5;
+	static int max = (int)1e5+2;
 	static int[] arr = new int[max];
-	
+	static ArrayList<Integer> primes = new ArrayList<>();
+
 	public static void sieve() {
 		for (int i=2; i<max; i++) {
 			arr[i] = i;
@@ -20,6 +20,28 @@ public class Sieve {
 					arr[j] = i;
 				}
 			}
+		}
+	}
+	
+	public static void getprimes() {
+		primes.add(2);
+		for (int i=2; i<max; i++) {
+			arr[i] = i;
+		}
+		for (int i=4; i<max; i+=2) {
+			arr[i] = 2;
+		}
+		int i=3;
+		for (; i*i<max; i+=2) {
+			if (arr[i] == i) {
+				primes.add(i);
+				for (int j= i*i; j < max; j+= i) {
+					arr[j] = i;
+				}
+			}
+		}
+		for (; i<max; i+=2) {
+			if (arr[i] == i) primes.add(i);
 		}
 	}
 	
