d05ae110435b7eab96f0221e14782cc552387c39
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-02-26 17:11:34 -0500

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -6,62 +6,38 @@ public class Hashing {
 
 	static long mod = (long)(1e9+7);
 	static long p = 97;
+	static int n;
+	static long[] pref, power;
+
 	
 	public static void main(String[] args) {
-
-	}
-	
-	public static long fullHash(String s) {
-		long hashval=0;
-		long pow = 1;
-		for (char c : s.toCharArray()) {
-			hashval = (hashval + (c - 'a' + 1) * pow)%mod;
-			pow *= p % mod;
-		}
-		return hashval;
-	}
-	
-	public static long[] prefHash(String s) {
-		long[] pref = new long[s.length()];
-		long hashval=0;
-		long pow=1;
-		int i=0;
-		for (char c : s.toCharArray()) {
-			hashval = (hashval + (c - 'a' + 1) * pow)%mod;
-			pow *= p % mod;
-			pref[i] = hashval;
-			i++;
-		}
-		return pref;
+		
 	}
 	
 		// hash of substring from i to j
 		// pref is array calculated in prefhash. Therefore only use this if you need
 			// to calculate hash of a lot of substrings
-	public static long SubstringHash(String s, int i, int j, long[] pref) {
-		if (i != 0) {
-	        return ((((pref[j] - pref[i-1])%mod+mod)%mod) * modInversePrime(pow(p, i, mod), mod))%mod;
-		}
-		else {
-			return (pref[j] * modInversePrime(pow(p, i, mod), mod))%mod;
-		}
+	public static long SubstringHash(String s, int i, int j) {
+	    if (i != 0) {
+	        return (((pref[j] - (pref[i-1] * power[j - i + 1])%mod)%mod+mod)%mod);
+	    }
+	    else {
+	        return pref[j];
+	    }
 	}
 	
-	static long modInversePrime(long a, long m) {
-        return pow(a, m - 2, m)%m;
-    }
-    
-    static long pow(long a, long b, long m) {
-    	long ans=1;
-    	while (b > 0) {
-    		if (b%2 == 1) {
-    			ans *= a;
-    			ans %= m;
-    		}
-    		a *= a;
-    		a %= m;
-    		b >>= 1;
-    	}
-    	return ans;
-    }
+	public static void prefHash(String s) {
+	    pref[0] = s.charAt(0) - 'a';
+	    for (int i=1; i<s.length(); i++) {
+	        pref[i] = ((pref[i-1]*p)%mod + (s.charAt(i) - 'a'))%mod;
+	    }
+	}
+	
+	public static void calc_power() {
+	    power[0] = 1;
+	    for (int i=1; i<n; i++) {
+	        power[i] = power[i-1] * p;
+	        power[i] %= mod;
+	    }
+	}
 }
