8d1c09c71dbc19b05e99bfaa39be5953423fb983
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-03-06 13:07:58 -0500

    Update Sieve.java

--- Sieve.java
+++ Sieve.java
@@ -4,20 +4,20 @@ import java.io.*;
 public class Sieve {
 	
 	static int max = (int)1e5+2;
-	static int[] arr = new int[max];
+	static int[] sieve = new int[max];
 	static ArrayList<Integer> primes = new ArrayList<>();
 
 	public static void sieve() {
 		for (int i=2; i<max; i++) {
-			arr[i] = i;
+			sieve[i] = i;
 		}
 		for (int i=4; i<max; i+=2) {
-			arr[i] = 2;
+			sieve[i] = 2;
 		}
 		for (int i=3; i*i <max; i+=2) {
-			if (arr[i] == i) {
+			if (sieve[i] == i) {
 				for (int j= i*i; j < max; j+= i) {
-					arr[j] = i;
+					sieve[j] = i;
 				}
 			}
 		}
@@ -26,22 +26,22 @@ public class Sieve {
 	public static void getprimes() {
 		primes.add(2);
 		for (int i=2; i<max; i++) {
-			arr[i] = i;
+			sieve[i] = i;
 		}
 		for (int i=4; i<max; i+=2) {
-			arr[i] = 2;
+			sieve[i] = 2;
 		}
 		int i=3;
 		for (; i*i<max; i+=2) {
-			if (arr[i] == i) {
+			if (sieve[i] == i) {
 				primes.add(i);
 				for (int j= i*i; j < max; j+= i) {
-					arr[j] = i;
+					sieve[j] = i;
 				}
 			}
 		}
 		for (; i<max; i+=2) {
-			if (arr[i] == i) primes.add(i);
+			if (sieve[i] == i) primes.add(i);
 		}
 	}
 	
@@ -49,6 +49,6 @@ public class Sieve {
 		
 		sieve();
 		
-		for (int i=0; i<max; i++) System.out.print(arr[i] + " ");
+		for (int i=0; i<max; i++) System.out.print(sieve[i] + " ");
 	}
 }
