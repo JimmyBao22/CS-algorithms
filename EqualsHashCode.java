0571cfff9cc2693086ca8b69ee5fed644e634e19
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-03-08 22:56:36 -0500

    Add files via upload

---
+++ EqualsHashCode.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.io.*;
+
+public class EqualsHashCode {
+
+	public static void main(String[] args) {
+
+	}
+	
+	static class A {
+		int a;
+		
+		@Override    
+		 public boolean equals(Object o) {        
+		     A cur = (A) o;   
+		     if (a != cur.a) return false;
+		     return true;
+		 }    
+		 
+		 @Override    
+		 public int hashCode() {        
+			 return a * 97 + 103;
+		 }
+	}
+	
+}
