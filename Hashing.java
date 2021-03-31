d9ad68377ef151ddd3ca3bed17e39f0937391ec2
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-03-31 19:35:51 -0400

    Update Hashing.java

--- Hashing.java
+++ Hashing.java
@@ -7,18 +7,18 @@ public class Hashing {
 	static long mod = (long)(1e9+7);
 	static long p = 97;
 	static int n;
-	static long[] pref, power;
-
+	static long[] pref, power, inv_power;
 	
 	public static void main(String[] args) {
 		pref = new long[n];
 		power = new long[n];
+		inv_power = new long[n];
+		
+	    // use the right ones together; either all '1' or all '2'
+				
 	}
 	
-		// hash of substring from i to j
-		// pref is array calculated in prefhash. Therefore only use this if you need
-			// to calculate hash of a lot of substrings
-	public static long SubstringHash(int i, int j) {
+	public static long SubstringHash1(int i, int j) {
 	    if (i != 0) {
 	        return (((pref[j] - (pref[i-1] * power[j - i + 1])%mod)%mod+mod)%mod);
 	    }
@@ -27,18 +27,60 @@ public class Hashing {
 	    }
 	}
 	
-	public static void prefHash(String s) {
+	public static void prefHash1(String s) {
 	    if (s.length() > 0) pref[0] = s.charAt(0) - 'a';
 	    for (int i=1; i<s.length(); i++) {
-	        pref[i] = ((pref[i-1]*p)%mod + (s.charAt(i) - 'a'))%mod;
+	        pref[i] = (pref[i-1]*p + (s.charAt(i) - 'a'))%mod;
 	    }
 	}
 	
-	public static void calc_power() {
-	    if (power.length > 0) power[0] = 1;
+	public static void calc_power1() {
+	    power[0] = 1;
 	    for (int i=1; i<n; i++) {
 	        power[i] = power[i-1] * p;
 	        power[i] %= mod;
 	    }
 	}
+	
+	
+	public static long SubstringHash2(int i, int j) {
+	    if (i != 0) {
+	        return ((((pref[j] - pref[i-1])%mod+mod)%mod) * inv_power[i])%mod;
+	    }
+	    else {
+	        return (pref[j] * inv_power[i])%mod;
+	    }
+	}
+
+	public static void prefHash2(String s) {
+	    if (s.length() > 0) pref[0] = s.charAt(0) - 'a';
+	    long pow = p;
+	    for (int i=1; i<(int)s.length(); i++) {
+	        pref[i] = (pref[i-1] + (s.charAt(i) - 'a') * pow)%mod;
+	        pow = (pow*p)%mod;
+	    }
+	}
+	
+	public static void calc_power2() {
+	    power[0] = inv_power[0] = 1;
+	    for (int i=1; i<n; i++) {
+	        power[i] = power[i-1] * p;
+	        power[i] %= mod;
+	        inv_power[i] = pow(power[i], mod-2, mod);
+	    }
+	}
+	
+	public static long pow(long a, long b, long m) {
+    	long ans = 1;
+    	while (b > 0) {
+    		if (b%2 == 1) {
+    			ans *= a;
+    			ans %= m;
+    		}
+    		a *= a;
+    		a %= m;
+    		b >>= 1;
+    	}
+    	return ans;
+    }
 }
